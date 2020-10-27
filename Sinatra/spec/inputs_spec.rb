55b23cd035efdf46d7eb6f20a67e48beb15c6606
Iliyan-Y <maximizebg@gmail.com>
2020-10-27 11:39:12 +0000

    RED parameters store in the object test

--- Sinatra/spec/inputs_spec.rb
+++ Sinatra/spec/inputs_spec.rb
@@ -7,4 +7,10 @@ describe Inputs do
     expect(db_values).to be_an_instance_of(Inputs)
   end
 
+  it "Inputs stores the key value pair instance of class Inputs" do
+    db_values = Inputs.new("user key", "user value")
+    expect(db_values.user_params[:"user key"]).to equal "user value"
+  end
+
+
 end
