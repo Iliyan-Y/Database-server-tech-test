fe06f09078b3d2c32f27bacbbeb114452a15f72f
Iliyan-Y <maximizebg@gmail.com>
2020-10-27 10:43:14 +0000

    RED get page test simplyfied

--- Sinatra/spec/features/get_page_spec.rb
+++ Sinatra/spec/features/get_page_spec.rb
@@ -1,8 +1,7 @@
 feature  "Get Page" do 
   
 
-  scenario "visit the get page" do
-    visit("/set?somekey=somevalue")
+  scenario "visit the get page expect to retunr the set value" do
     visit("/get?key=somekey")
     expect(page).to have_content "somevalue"
   end
