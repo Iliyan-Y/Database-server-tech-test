f858b8220d594fde9d28cbec9b96b39095bc1757
Iliyan-Y <maximizebg@gmail.com>
2020-10-27 12:24:15 +0000

    GREEN get page display correct value

--- Sinatra/app.rb
+++ Sinatra/app.rb
@@ -1,16 +1,20 @@
 require 'sinatra/base'
+require './lib/inputs'
 
 class ServerTest < Sinatra::Base
+  enable :sessions
+
   get '/' do
     'HELOOO SINATRA'
   end
 
   get '/set' do
+    session[:inputs] = Inputs.new(params.keys[0], params.values[0])
     'HELOOO SET PAGE'
   end
 
   get '/get' do
-    'somevalue'
+    "#{session[:inputs].user_params[params.values[0]]}"
   end
 
 
