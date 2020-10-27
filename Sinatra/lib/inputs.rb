ff077e795a0b853a158092c32e704484d6b8330e
Iliyan-Y <maximizebg@gmail.com>
2020-10-27 11:48:22 +0000

    GREEN parameters store in the object test

--- Sinatra/lib/inputs.rb
+++ Sinatra/lib/inputs.rb
@@ -1,3 +1,9 @@
 class Inputs
+
+  attr_reader :user_params
+
+  def initialize(user_key, user_value) 
+    @user_params = {user_key => user_value}
+  end
   
 end
--- Sinatra/spec/inputs_spec.rb
+++ Sinatra/spec/inputs_spec.rb
@@ -2,14 +2,10 @@ require 'inputs.rb'
 
 describe Inputs do 
 
-  it "create instance of class Inputs" do
-    db_values  = Inputs.new
-    expect(db_values).to be_an_instance_of(Inputs)
-  end
 
   it "Inputs stores the key value pair instance of class Inputs" do
     db_values = Inputs.new("user key", "user value")
-    expect(db_values.user_params[:"user key"]).to equal "user value"
+    expect(db_values.user_params["user key"]).to eq "user value"
   end
 
 
