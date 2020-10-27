f776f59c62c22354297dac28c816cc1f3e9e3316
Iliyan-Y <maximizebg@gmail.com>
2020-10-27 11:51:45 +0000

    RED get page display correct value

--- Sinatra/spec/features/get_page_spec.rb
+++ Sinatra/spec/features/get_page_spec.rb
@@ -1,9 +1,15 @@
 feature  "Get Page" do 
   
-
   scenario "visit the get page expect to retunr the set value" do
+    visit("/set?somekey=somevalue")
     visit("/get?key=somekey")
     expect(page).to have_content "somevalue"
+  end
+
+  scenario "visit the get page expect to retunr different set value" do
+    visit("/set?somekey=value2")
+    visit("/get?key=somekey")
+    expect(page).to have_content "value2"
   end
 
 end
--- Sinatra/spec/features/home_spec.rb
+++
@@ -1,6 +0,0 @@
-feature "View Home Page" do 
-  scenario "visit the home page" do
-    visit("/")
-    expect(page).to have_content "HELOOO SINATRA"
-  end
-end
